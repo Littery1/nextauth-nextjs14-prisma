6205e1db67596bb6eb1d92b2e52c2e0478565e20
Littery <93970315+Littery1@users.noreply.github.com>
2024-10-31 08:41:05 +0100

    Setup

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <section className='bg-ct-blue-600 min-h-screen pt-20'>
         <div className='max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center'>
           <p className='text-3xl font-semibold'>
-            Implement Authentication with NextAuth in Next.js 14
+            Welcome to SourceSeekr
           </p>
         </div>
       </section>
